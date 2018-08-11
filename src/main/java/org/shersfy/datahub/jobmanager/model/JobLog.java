be796b9cd26b05d4196f3eafc06f28c35f1d5ce5
pengyang <pengyang@cmhi.chinamobile.com>
2018-08-12 04:07:39 +0800

    job log

--- src/main/java/org/shersfy/datahub/jobmanager/model/JobLog.java
+++ src/main/java/org/shersfy/datahub/jobmanager/model/JobLog.java
@@ -1,24 +1,17 @@
 package org.shersfy.datahub.jobmanager.model;
 
-import java.util.Date;
 
 public class JobLog extends BaseEntity {
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
     /** 任务ID **/
     private Long jobId;
 
     /** 结果状态(0：执行中(默认)，1：执行成功，2：执行失败) **/
     private Integer status;
-
-    /** 开始时间 **/
-    private Date startTime;
-
-    /** 结束时间 **/
-    private Date endTime;
 
     /** 日志存放路径 **/
     private String path;
@@ -37,22 +30,6 @@ public class JobLog extends BaseEntity {
 
     public void setStatus(Integer status) {
         this.status = status;
-    }
-
-    public Date getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    public Date getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
     }
 
     public String getPath() {
