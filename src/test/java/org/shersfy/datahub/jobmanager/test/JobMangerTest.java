55415567d2a2bc28879b1df6d743d221144b6767
pengyang <pengyang@cmhi.chinamobile.com>
2018-08-14 01:34:48 +0800

    test

--- src/test/java/org/shersfy/datahub/jobmanager/test/JobMangerTest.java
+++ src/test/java/org/shersfy/datahub/jobmanager/test/JobMangerTest.java
@@ -3,6 +3,7 @@ package org.shersfy.datahub.jobmanager.test;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.shersfy.datahub.commons.utils.AesUtil;
 
 
 public class JobMangerTest extends BaseTest{
@@ -24,6 +25,13 @@ public class JobMangerTest extends BaseTest{
     public void test03() throws IOException {
         String test = "test3.txt";
         testCase(test);
+    }
+    
+    @Test
+    public void test04() throws IOException {
+        String test = "test4.txt";
+        System.out.println(test);
+        System.out.println(AesUtil.encryptStr("123456", AesUtil.AES_SEED));
     }
     
 }
