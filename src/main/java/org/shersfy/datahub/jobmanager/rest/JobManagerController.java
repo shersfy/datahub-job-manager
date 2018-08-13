c40f32a2d262cc220260ed1764832920914b5aa5
pengyang <pengyang@cmhi.chinamobile.com>
2018-08-13 17:35:07 +0800

    refresh

--- src/main/java/org/shersfy/datahub/jobmanager/rest/JobManagerController.java
+++ src/main/java/org/shersfy/datahub/jobmanager/rest/JobManagerController.java
@@ -12,11 +12,13 @@ import org.shersfy.datahub.jobmanager.service.JobInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class JobManagerController extends BaseController{
     
     protected Logger LOGGER = LoggerFactory.getLogger(getClass());
